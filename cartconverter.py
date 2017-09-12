5d88f578723ccd2974844266901a4c95a8000d2d
Rahul Aedula <rahulaedula95@gmail.com>
2017-09-12 19:29:15 +0530

    1st Pass

---
+++ cartconverter.py
@@ -0,0 +1,100 @@
+import pandas as pd 
+import numpy as np
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from joblib import Parallel, delayed
+import multiprocessing
+#import copy
+
+def filereader(filename):
+
+
+	df1 = pd.read_csv(filename)
+	return df1
+
+
+
+def degreetocart(data_f1):
+	
+	global df2
+	df2 = data_f1.copy()
+	print "phase 1"
+
+	df2['X'] = np.nan
+	df2['Y'] = np.nan
+	df2['Z'] = np.nan
+	df2 = df2.astype(float)
+
+	print "phase 2"
+	num_cores = multiprocessing.cpu_count()
+	
+	
+	results_x = Parallel(n_jobs=num_cores)(delayed(xloop)(i) for i in xrange(0,len(df2)))
+	print "phase 3"
+	#print results_x
+
+	#print results_x
+	
+	#print " this is "
+	#print results_x[0]
+
+	results_y = Parallel(n_jobs=num_cores)(delayed(yloop)(i) for i in xrange(0,len(df2)))
+	print "phase 4"
+	results_z = Parallel(n_jobs=num_cores)(delayed(zloop)(i) for i in xrange(0,len(df2)))
+	print "phase 5"
+	#print results_y
+
+
+
+	#Parallel(n_jobs=num_cores)(delayed(adjloop)(i) for i in xrange(0,len(df2)))
+	for i in xrange(0,len(df2)):
+		print i
+		df2['X'][i] = results_x[i]
+		df2['Y'][i] = results_y[i]
+		df2['Z'][i] = results_z[i]
+
+
+def xloop(i):
+	c = SkyCoord(ra=df2['alpha_j2000'][i]*u.degree, dec=df2['delta_j2000'][i]*u.degree)
+	#print i
+	val = c.cartesian.x
+	return float(val)
+
+def zloop(i):
+	c = SkyCoord(ra=df2['alpha_j2000'][i]*u.degree, dec=df2['delta_j2000'][i]*u.degree)
+	#print i
+	val = c.cartesian.z
+	return float(val)
+
+
+	#print val
+	#print df2['mmm'][i]
+
+def yloop(i):
+	c = SkyCoord(ra=df2['alpha_j2000'][i]*u.degree, dec=df2['delta_j2000'][i]*u.degree)
+	#print i
+	val = c.cartesian.y
+	return float(val)
+
+def filewriter(data_f2):
+
+	writename = '~/Data/DATA2.csv'
+
+	data_f2.to_csv(writename,sep=',')
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+	filename = '~/Data/NewDataset3.csv'
+	
+	data_frame1 = filereader(filename)
+
+	
+	degreetocart(data_frame1)
+	filewriter(df2)
+	#print df2
---
+++ pass1.py
@@ -0,0 +1,129 @@
+import pandas as pd 
+import numpy as np 
+import math
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from joblib import Parallel, delayed
+import multiprocessing
+
+
+
+def filereader(filename):
+
+
+	df1 = pd.read_csv(filename)
+	return df1
+
+
+def pass1(i,j):
+
+	print i,j
+	arr = []
+	x = df['X'][i] * df['X'][j]
+
+	y = df['Y'][i] * df['Y'][j]
+
+	z = df['Z'][i] * df['Z'][j]
+
+	tot = x + y + z
+
+	if tot >1: 
+		tot = 1
+
+	angle = math.acos(tot)
+
+	arcsec = angle * ((180/math.pi) * 3600)
+
+	'''
+	print arcsec
+	'''
+	#print angle,arcsec," i j ", i , j
+	
+	if arcsec < 5 :
+	 	return (i,j)
+	else:
+
+		 return None
+
+
+def filewriter(data_f2,data_f3):
+
+	writename = '~/Data/NonDuplicate.csv'
+
+	data_f2.to_csv(writename,sep=',')	
+				
+	writename2 = '~/Data/Duplicate.csv'
+
+	data_f3.to_csv(writename2,sep=',')	
+	
+
+
+def data_split(arr):
+
+	'''
+	num2 = df.values 
+
+	
+	num2 = np.delete(num2,)
+	'''
+
+		
+
+	df2 = df
+
+	df3 = df
+	#print arr
+
+	df2 = df2.drop([i for i in arr])
+
+	df3 = df3.drop([i for i in xrange(0,len(df)) if i not in arr])
+	
+	
+	return (df2,df3)
+
+
+
+
+
+if __name__ == "__main__":
+
+	filename = '~/Data/DATA2.csv'
+	
+	global df 
+
+	df = filereader(filename)
+	
+	num_cores = multiprocessing.cpu_count()
+	
+	results_i  = Parallel(n_jobs=num_cores, backend="multiprocessing")(delayed(pass1)(i,j) for i in xrange(0,2) for j in xrange((i+1),(len(df)))) #xrange((i+1)
+
+	#results2 = way2(data_frame1)
+	#print results_i
+	
+
+	results_i = np.unique(results_i)
+
+	#print results_i
+	results_i =  np.delete(results_i,0)
+
+	liss_i = [ x[0] for x in results_i ]
+	liss_i = np.asarray(liss_i)
+	liss_j = [ x[1] for x in results_i]
+	liss_j = np.asarray(liss_j)
+	
+	results_i = np.concatenate([liss_i,liss_j])
+
+	
+	results_i = np.unique(results_i)
+	#print results_i
+	
+	#print len(results_i)
+
+	#print results_i
+
+	data_frame2 , data_frame3 = data_split(results_i)
+
+	filewriter(data_frame2,data_frame3)
+
+	
+	#print results_i
